8112c1ab39e83871c88c4f6c4470208379107b89
misha <ilayfeeed@gmail.com>
2023-04-26 17:48:45 +0300

    requests

--- bot/backrequests.py
+++ bot/backrequests.py
@@ -12,13 +12,13 @@ from typing import Union, Any
 base_url = "http://127.0.0.1:8000/"
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-async def add_user(user: Union[Driver, Passenger]) -> bool:
+def add_user(user: Union[Driver, Passenger]) -> bool:
     response = requests.post(url=base_url + 'user', data=json.dumps(user.dict(), default=str))
     response = json.loads(response.text)
     return response["msg"] == "success"
 
 
-async def get_user_from_db(tg_id: int) -> Union[Driver, Passenger]:
+def get_user_from_db(tg_id: int) -> Union[Driver, Passenger]:
     response = requests.get(url=base_url + "user", params={"tg_id": tg_id})
     response = json.loads(response.text)
     if "places" in response:
@@ -26,19 +26,19 @@ async def get_user_from_db(tg_id: int) -> Union[Driver, Passenger]:
     return Passenger(**response)
 
 
-async def patch_user_from_db(user: Union[Driver, Passenger]) -> bool:
+def patch_user_from_db(user: Union[Driver, Passenger]) -> bool:
     response = requests.patch(url=base_url + 'user', data=json.dumps(user.dict(), default=str))
     response = json.loads(response.text)
     return response["msg"] == "success"
 
 
-async def add_driver_to_query(driver_tg_id: int) -> bool:
+def add_driver_to_query(driver_tg_id: int) -> bool:
     response = requests.get(url=base_url + "add_driver_to_query", params={"driver_tg_id": driver_tg_id})
     response = json.loads(response.text)
     return response["msg"] == "success"
 
 
-async def find_driver_for_passenger(passenger_tg_id: int) -> Any[Driver, bool]:
+def find_driver_for_passenger(passenger_tg_id: int) -> Any[Driver, bool]:
     response = requests.get(url=base_url + "find_driver", params={"passenger_tg_id": passenger_tg_id})
     response = json.loads(response.text)
     if "msg" in response:
